7096bd6366d85112728d46a6168ab832dfabc63b
bigdongdong <qipq@qq.com>
2020-12-29 11:58:29 +0800

    glide-4.9-sdk-1.2.3-version-1.0

--- app/src/main/java/com/cxd/huanxin_kefu_easeu/MainActivity.java
+++ app/src/main/java/com/cxd/huanxin_kefu_easeu/MainActivity.java
@@ -1,8 +1,8 @@
 package com.cxd.huanxin_kefu_easeu;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
