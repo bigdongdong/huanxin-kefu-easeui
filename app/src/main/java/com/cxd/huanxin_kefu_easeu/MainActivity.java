ac7326ad298883e412818ea83133b4779e01e322
bigdongdong <qipq@qq.com>
2020-12-29 10:51:24 +0800

    init

---
+++ app/src/main/java/com/cxd/huanxin_kefu_easeu/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cxd.huanxin_kefu_easeu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
